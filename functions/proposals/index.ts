28061e96532c994282561aed48decb222157a8d0
marmotmarsh <marmotmarsh74@gmail.com>
2023-01-24 08:26:57 -0800

    test some things

--- functions/proposals/index.ts
+++ functions/proposals/index.ts
@@ -1,12 +1,14 @@
 import { Handler } from '@netlify/functions';
 
 export const handler: Handler = async (event, context) => {
-  const { name = 'stranger' } = event.queryStringParameters;
+  // const { name = 'stranger' } = event.queryStringParameters;
+  const proposal = event.body;
+  const method = event.httpMethod;
 
   return {
     statusCode: 200,
     body: JSON.stringify({
-      message: `Hello, ${name}!`,
+      message: `Hello, you used method: ${method}!`,
     }),
   };
 };
