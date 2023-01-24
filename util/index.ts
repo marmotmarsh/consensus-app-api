f1032b82fdb6e211fb567aabade4c1bf58f42073
marmotmarsh <marmotmarsh74@gmail.com>
2023-01-24 10:27:43 -0800

    reconfigure db connection options

--- util/index.ts
+++ util/index.ts
@@ -1,10 +1,19 @@
-import mysql, { Connection } from 'mysql2';
+import mysql, { Connection, ConnectionOptions } from 'mysql2';
 
 export function createDBConnection(): Connection {
+  const connectionOptions: ConnectionOptions = {
+    user: process.env.DATABASE_USER,
+    password: process.env.DATABASE_PASSWORD,
+    database: process.env.DATABASE_NAME,
+    host: process.env.DATABASE_HOST,
+    ssl: {
+      rejectUnauthorized: true,
+    },
+  };
   const url = process.env.DATABASE_URL;
   if (url === undefined) {
     throw new Error('Server Error');
   }
-  const connection = mysql.createConnection(url);
+  const connection = mysql.createConnection(connectionOptions);
   return connection;
 }
