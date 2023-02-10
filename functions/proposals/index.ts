705a8f12906db49faaf32fd767dc035be15c4881
marmotmarsh <marmotmarsh74@gmail.com>
2023-02-10 08:57:33 -0800

    netlify doesn't do toml headers for some reason, will manually add them to responses

--- functions/proposals/index.ts
+++ functions/proposals/index.ts
@@ -5,20 +5,29 @@ export const handler: Handler = async (event, context) => {
   const { path, httpMethod } = event;
   const subPaths = path.split('/');
 
-  // Create New Proposal
-  if (
-    subPaths[0] === '' &&
-    subPaths[1] === '.netlify' &&
-    subPaths[2] === 'functions' &&
-    subPaths[3] === 'proposals' &&
-    subPaths[4] === 'create' &&
-    httpMethod === 'POST'
-  ) {
-    return await createProposal(event, context);
+  if (subPaths[1] === '.netlify' && subPaths[2] === 'functions') {
+    switch (httpMethod) {
+      case 'POST': {
+        // Create New Proposal
+        if (subPaths[3] === 'proposals' && subPaths[4] === 'create') {
+          return await createProposal(event, context);
+        }
+      }
+      case 'OPTIONS': {
+        // Pre-Flight Response
+        return {
+          statusCode: 200,
+          headers: {
+            'Access-Control-Allow-Origin': 'https://example.com',
+            'Access-Control-Allow-Methods': 'GET, DELETE, HEAD, OPTIONS',
+          },
+        };
+      }
+    }
   }
 
   return {
-    statusCode: 200,
+    statusCode: 400,
     body: JSON.stringify({
       message: `Hello, you used method: ${httpMethod} and path: ${path}, which do not exist`,
     }),
